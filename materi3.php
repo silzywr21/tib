a94f3a47ca3197a9366ba76990ac5435d96091c4
silzywr21 <silzy7012@gmail.com>
2026-04-27 14:56:02 +0800

    Update materi3.php

--- materi3.php
+++ materi3.php
@@ -1,15 +1,54 @@
 <?php
-function sapa($nama) {
-    echo "Halo, $nama";
+
+function salam()
+{
+    echo "assalamualaikum " ;
 }
 
-sapa("silzy");
+salam();
+
+
+function tambah(int$a, int$b)
+{
+    $jumlah = $a + $b;
+    echo $jumlah; 
+}
+
+function kali(int$a, int$b)
+{
+    $jumlah = $a * $b;
+    echo $jumlah; 
+}
+function kurang(int $a, int $b)
+{
+    $hasil = $a - $b;
+    echo $hasil;
+}
+function bagi(int $a, int $b)
+{
+    $hasil = $a / $b;
+    echo $hasil;
+}
 ?>
-<?php
-function tambah($a, $b) {
-    return $a + $b;
-}
 
-$hasil = tambah(5, 3);
-echo "Hasil: $hasil";
+<form method="POST">
+    <input type="number" name="angka1">
+    <input type="number" name="angka2">
+    <button type="submit"name="kirim">kirim</button>
+</form>
+
+<?php
+
+if (isset($_POST['kirim'])) {
+    $angka1 = $_POST['angka1'];
+    $angka2 = $_POST['angka2'];
+    tambah($angka1, $angka2);
+    echo "<br><br>";
+    kali($angka1, $angka2);
+    echo "<br><br>";
+    kurang($angka1, $angka2);
+    echo "<br><br>";
+    bagi($angka1, $angka2);
+    
+}
 ?>
